fb1e0d05b9c9d0f995d4b0a5f6e61b1aa589bcf1
Qing, Lv <luqin@airbiquity.com>
2014-04-08 16:13:10 +0800

    delete CI and update html

---
+++ resource/common.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+	$(".col3 li:nth-child(3), .col3 div:nth-child(3)").css({"float":"right","width":"250px"});
+	$(".col4 li:nth-child(4), .col4 div:nth-child(4)").css({"float":"right","width":"180px"});
+	$(".col3 li:nth-child(3) p, .col4 li:nth-child(4) p").css({"padding-right":"0"});
+	$(".list li:last-child").css({"padding-right":"0"});
+	$("#footer ul.pageLink li:last-child").css({"border-right":"none"});
+
+	var $k = $(".pNav li").length;
+	$(".pNav ul").addClass("li"+($k-2));
+
+	$("a[href*='.pdf'] ,a[href^='http://']").click(function(){ this.target = "_blank"; });
+
+	if (navigator.platform.indexOf("Win") != -1) {
+		$('head').append('<link rel="stylesheet" href="/common/css/win.css" type="text/css" media="all" />');
+	}
+});
---
+++ resource/lang.js
@@ -0,0 +1,83 @@
+LANG_dragdropplus={
+    empty_box:'空板块区域'
+};
+
+LANG_formplus={
+    days:['日', '一', '二', '三', '四', '五', '六'],
+    months:['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    year: '年',
+    dateerror:'日期无效!',
+    validate:{
+        required:'本项必填',
+        minLength: ' ',
+        maxLength: ' ',
+        number:'请填写数值',
+        msn:'请填写MSN',
+        skype:'请填写Skype',
+        digits:'请填写整数',
+        unsignedint:'请填写正整数',
+        unsigned:'请填写大于等于0的数值',
+        positive:'请填写大于0的数值',
+        alpha:'请填写英文字母',
+        alphaint:'请填写英文字母或者数字',
+        alphanum:'请填写英文字母、中文及数字',
+        unzhstr:'不能填写中文字符',
+        date:'请填写日期，格式yyyy-mm-dd',
+        email:'请填写正确的email地址',
+        mobile:'请填写正确的手机号码',
+        tel:'请填写正确的固定电话',
+        phone:'请填写正确的电话或手机',
+        zip:'请填写正确的邮编',
+        url:'请填写正确的网址',
+        area:'请选择完整的地区',
+        equal:'两次填写不一致',
+        onerequired:'请至少选择一项'
+    },
+    formValError:'表单验证失败'
+
+};
+LANG_shopwidgets={
+    dropghostTitle1:'点击此处，将',
+    dropghostTitle2:'版块添加到此区域',
+    addWidget:'增加版块&raquo;',
+    editWidget:'修改版块&raquo;',
+    editHTML:'编辑HTML',
+    saving:'正在保存...',
+    saveSuccess:'保存成功!',
+    saveError:'保存失败',
+    comfirmDel:'确定删除此版块?',
+    tip:'友情提示'
+};
+
+LANG_jstools = {
+    messageSuccess:'操作成功!',
+    messageError:'操作失败!'
+};
+
+LANG_goodscupcake={
+    goodsCompare:'商品对比',
+    close:'关闭',
+    del:'删除',
+    compare:'对比',
+    clear:'清空',
+    isset:'该商品已经放入对比栏',
+	errortype:'只能对比同类的商品',
+    lengtherror:'最多只能对比5个商品',
+    minlengtherror:'最少需要对比2个商品'
+};
+
+
+LANG_goodseditor={
+    comfirm:'\t是否根据所选分类的默认类型重新设定商品类型？\n\n如果重设，可能丢失当前所输入的类型属性、关联品牌、参数表等类型相关数据。',
+    editvipprice:'编辑会员价',
+    type:'规格',
+    widget:'配件',
+    comfirmDelPic:'确认删除本图片吗?',
+    viewSource:'查看图片信息'
+};
+
+LANG_spec={
+    select:'请选择',
+    select_spec:'请选择规格:'
+};
+
